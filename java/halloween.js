a83717374f4216815c4ea374089b0e0b5c0f37a3
Alma Ordaz <ordazalmadulcegmail.com>
2021-11-26 10:09:31 -0800

    scrolldelay halloween final

---
+++ java/halloween.js
@@ -0,0 +1,18 @@
+/*window.onscroll = function(){
+    if(document.documentElement.scrolling > 100){
+        document.querySelector(".go-top-container")
+        .classlist.add("show");
+    }else{
+
+        document.querySelector(".go-top-container")
+        .classlist.remove("show");
+    }
+}*/
+
+ScrollReveal().reveal('.logo' , {delay : 800});
+ScrollReveal().reveal('.cupcake_claro' , {delay : 800});
+ScrollReveal().reveal('.dulzura' , {delay : 600});
+ScrollReveal().reveal('.imagen_cupcakes_vainilla' , {delay : 800});
+ScrollReveal().reveal('.dark-mode' , {delay : 700});
+ScrollReveal().reveal('.lightmode' , {delay : 800});
+ScrollReveal().reveal('.cupcake-azul' , {delay : 800});
